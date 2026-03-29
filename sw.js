5b0c709356a2563b48d40bc701fea5f50f4c1f83
philhorner88 <35714827+philhorner88@users.noreply.github.com>
2026-03-29 19:49:03 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,43 @@
-const CACHE_NAME = 'repvault-v16b';
+const CACHE_NAME = ‘repvault-v17’;
 
 // Install: cache core assets
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll([
-      './',
-      './index.html',
-      './manifest.json',
-      './icon-192.png',
-      './icon-512.png'
-    ]).catch(() => {}))
-  );
+self.addEventListener(‘install’, e => {
+self.skipWaiting();
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll([
+‘./’,
+‘./index.html’,
+‘./manifest.json’,
+‘./icon-192.png’,
+‘./icon-512.png’
+]).catch(() => {}))
+);
 });
 
 // Activate: clear old caches
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
-  );
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(keys => Promise.all(
+keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+)).then(() => self.clients.claim())
+);
 });
 
 // Fetch: NETWORK-FIRST strategy
 // Always try network first, fall back to cache for offline support
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  if (!e.request.url.startsWith(self.location.origin)) return;
+self.addEventListener(‘fetch’, e => {
+if (e.request.method !== ‘GET’) return;
+if (!e.request.url.startsWith(self.location.origin)) return;
 
-  e.respondWith(
-    fetch(e.request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        return response;
-      })
-      .catch(() => {
-        return caches.match(e.request);
-      })
-  );
+e.respondWith(
+fetch(e.request)
+.then(response => {
+const clone = response.clone();
+caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+return response;
+})
+.catch(() => {
+return caches.match(e.request);
+})
+);
 });
